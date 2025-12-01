748191fd411ce59d005ce5b15a3d119fd0e75e89
Adi Maitre <adimaitre56@gmail.com>
2025-12-01 17:28:49 +0530

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { PortfolioDock } from './components/PortfolioDock';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PortfolioDock />
       <main>
         <Hero />
         <About />
